c14cd84d51223667d20f88aa97460175fe041019
Diversity <karim.1030@live.fr>
2023-12-22 21:43:14 +0100

    Initial commit

---
+++ src/main/java/net/diversity/ultimatemod/UltimateModClient.java
@@ -0,0 +1,11 @@
+package net.diversity.ultimatemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class UltimateModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient()
+    {
+
+    }
+}
